930c9c4a25dbbbfae9dbbe1293c75868fdf7ef6d
garsnow <garsnow@princeton.edu>
2024-12-10 11:26:56 -0500

    Update test_MB_dist.py

--- tests/test_MB_dist.py
+++ tests/test_MB_dist.py
@@ -2,13 +2,13 @@ import numpy as np
 import pytest
 from scipy.stats import kstest
 
-from src.example.MD_dist import MDSimulation, get_speeds
+from src.example.MD_dist import MDSimulation, get_speeds, particle_simulator
 
 cutoff = 0.05
 
 
 def is_maxwell_boltzmann(speeds: np.ndarray, m: float, T: float) -> bool:
-    '''
+    """
     Checks if the speed distribution from the MB_dist simulation is sampled from a Maxwell_Boltzmann distribution. 
 
     Args:
@@ -18,7 +18,7 @@ def is_maxwell_boltzmann(speeds: np.ndarray, m: float, T: float) -> bool:
 
     Returns:
         bool
-    '''
+    """
     kb = 1.38e-23
     a = np.sqrt(m / (2 * kb * T))
     cdf = lambda v: (2 / np.sqrt(np.pi)) * (a**3) * v**2 * np.exp(-a**2 * v**2)
@@ -27,37 +27,8 @@ def is_maxwell_boltzmann(speeds: np.ndarray, m: float, T: float) -> bool:
 
 
 @pytest.fixture
-def setup_simulation() -> tuple:
-    '''
-    Initializes simulation. 
-
-    Args:
-        None
-
-    Returns:
-        sim
-        dt
-        m
-
-    '''
-    n = 1000
-    rscale = 5.0e6
-    r = 2e-10 * rscale
-    tscale = 1e9
-    sbar = 353 * rscale / tscale
-    dt = 1 / 30
-    m = 1
-    T = 300
-    pos = np.random.random((n, 2))
-    theta = np.random.random(n) * 2 * np.pi
-    s0 = sbar * np.random.random(n)
-    vel = (s0 * np.array((np.cos(theta), np.sin(theta)))).T
-    sim = MDSimulation(pos, vel, r, m)
-    return sim, dt, m, T  # Return the simulation, time step, mass, and temperature
-
-
-def test_maxwell_boltzmann_distribution(setup_simulation: tuple) -> None:
-    '''
+def test_maxwell_boltzmann_distribution() -> None:
+    """
     Checks if the speed distribution from the MB_dist simulation is sampled from a Maxwell_Boltzmann distribution. 
 
     Args:
@@ -65,12 +36,13 @@ def test_maxwell_boltzmann_distribution(setup_simulation: tuple) -> None:
 
     Returns:
         None
-    '''
-    sim, dt, m, T = setup_simulation
-    for _ in range(1000):  # Run the simulation for 1000 steps
-        sim.advance(dt)
+    """
+    number_of_particles = 1000
+    time_step = 30
+    particle_mass = 1
+    sim = particle_simulator(number_of_particles, time_step, particle_mass)
     speeds = get_speeds(sim.vel)
-    assert is_maxwell_boltzmann(speeds, m, T), "Does not match Maxwell-Boltzmann distribution"
+    assert is_maxwell_boltzmann(speeds, particle_mass, T=300), "Does not match Maxwell-Boltzmann distribution"
 
 
 if __name__ == "__main__":
