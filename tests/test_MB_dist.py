fabe891af7e51936a4d074a08b3d6ae790797708
garsnow <garsnow@princeton.edu>
2024-12-10 11:04:22 -0500

    Add docstrings and static typing to test_MB_dist.py

--- tests/test_MB_dist.py
+++ tests/test_MB_dist.py
@@ -7,7 +7,18 @@ from src.example.MD_dist import MDSimulation, get_speeds
 cutoff = 0.05
 
 
-def is_maxwell_boltzmann(speeds, m, T):
+def is_maxwell_boltzmann(speeds: np.ndarray, m: float, T: float) -> bool:
+    '''
+    Checks if the speed distribution from the MB_dist simulation is sampled from a Maxwell_Boltzmann distribution. 
+
+    Args:
+        speeds (array): speeds of particles.
+        m (float): particle mass (assumed identical between particles).
+        T (float): temperature.
+
+    Returns:
+        bool
+    '''
     kb = 1.38e-23
     a = np.sqrt(m / (2 * kb * T))
     cdf = lambda v: (2 / np.sqrt(np.pi)) * (a**3) * v**2 * np.exp(-a**2 * v**2)
@@ -16,7 +27,19 @@ def is_maxwell_boltzmann(speeds, m, T):
 
 
 @pytest.fixture
-def setup_simulation():
+def setup_simulation() -> tuple:
+    '''
+    Initializes simulation. 
+
+    Args:
+        None
+
+    Returns:
+        sim
+        dt
+        m
+
+    '''
     n = 1000
     rscale = 5.0e6
     r = 2e-10 * rscale
@@ -24,15 +47,25 @@ def setup_simulation():
     sbar = 353 * rscale / tscale
     dt = 1 / 30
     m = 1
+    T = 300
     pos = np.random.random((n, 2))
     theta = np.random.random(n) * 2 * np.pi
     s0 = sbar * np.random.random(n)
     vel = (s0 * np.array((np.cos(theta), np.sin(theta)))).T
     sim = MDSimulation(pos, vel, r, m)
-    return sim, dt, m, 300  # Return the simulation, time step, mass, and temperature
+    return sim, dt, m, T  # Return the simulation, time step, mass, and temperature
 
 
-def test_maxwell_boltzmann_distribution(setup_simulation):
+def test_maxwell_boltzmann_distribution(setup_simulation: tuple) -> None:
+    '''
+    Checks if the speed distribution from the MB_dist simulation is sampled from a Maxwell_Boltzmann distribution. 
+
+    Args:
+        setup_simulation
+
+    Returns:
+        None
+    '''
     sim, dt, m, T = setup_simulation
     for _ in range(1000):  # Run the simulation for 1000 steps
         sim.advance(dt)
