cc42a20c2ee5f5e5d540d4a974c8887896e9936b
garsnow <garsnow@princeton.edu>
2024-12-11 10:47:56 -0500

    Remove species_and_particle.py

--- src/MB_dist.py
+++ src/MB_dist.py
@@ -7,10 +7,36 @@ from matplotlib import patches, path
 from matplotlib.animation import FuncAnimation
 from scipy.spatial.distance import pdist, squareform
 
-#imports classes needed
-from src.species_and_particle import Species, Particle
-
 X, Y = 0, 1
+
+class Species: 
+    #defines each species to have common attributes: name, mass, radius, color
+    
+    def __init__(self, name, mass, radius, color):
+        self.name = name
+        self.mass = mass
+        self.radius = radius
+        self.color = color
+
+class Particle: 
+    #defines a particle which is an instance of a species
+    
+    def __init__(self, species, pos, vel):
+        self.species = species 
+        self.pos = np.array(pos, dtype = float)
+        self.vel = np.array(vel, dtype = float)
+
+    @property
+    def mass(self):
+        return self.species.mass
+
+    @property
+    def radius(self):
+        return self.species.radius
+
+    @property
+    def color(self):
+        return self.species.color
 
 class MDSimulation:
 
