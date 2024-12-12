51a8589cd2b0c0e7abf65fb71896e29928660931
JuliaBaratta <142746100+JuliaBaratta@users.noreply.github.com>
2024-12-12 18:49:09 -0500

    Update MB_dist.py

--- src/MB_dist.py
+++ src/MB_dist.py
@@ -267,7 +267,7 @@ class Histogram:
         Args:
             ax: matplotlib axes object
         """
-        codes: NDArray[np.int_] = np.ones(self.nverts, dtype=int) * path.Path.LINETO
+        codes: NDArray[np.intp] = np.full(self.nverts, path.Path.LINETO, dtype=np.intp)
         codes[0::5] = path.Path.MOVETO
         codes[4::5] = path.Path.CLOSEPOLY
         barpath: path.Path = path.Path(self.verts, codes)
